b658043270a45d275a9158b65d148ea6ed78375d
Vitalyy Pikozh <wisart@gmail.com>
2021-08-21 00:26:55 +0300

    set loadash and pnotify

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 import './sass/main.scss';
+import { alert, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
