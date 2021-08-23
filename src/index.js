37ad085548c53cadb41d16f4ff27172a8f3a5b82
Vitalyy Pikozh <wisart@gmail.com>
2021-08-23 23:52:17 +0300

    add debounce

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './sass/main.scss';
 import { alert, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
 import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
+import debounce from 'lodash.debounce';
+
+export default class newFoundedCoutry {
+  constructor() {}
+
+  fetchCountry() {
+    const url = 'https://restcountries.eu/rest/v2/name/{name}';
+    return fetch(url)
+      .then(response => response.json())
+      .then();
+  }
+}
